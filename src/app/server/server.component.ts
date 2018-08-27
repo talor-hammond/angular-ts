86abf99acc0763fb80b66f4495a7d53920d7e97b
talor-hammond <talorjhammond@gmail.com>
2018-08-28 09:29:52 +1200

    initialise first component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core' // decorator that marks a class as an angular component...
+
+@Component({
+    selector: 'app-server', // convention: `app-${componentName}`
+    templateUrl: './server.component.html'
+})
+
+export class AppComponent {
+    
+}
