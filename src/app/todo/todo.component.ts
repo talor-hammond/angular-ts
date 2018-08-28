89ef118ef356e9a570d9f6e5dd3a2d04c4a2ecbb
talor-hammond <talorjhammond@gmail.com>
2018-08-28 13:02:23 +1200

    directive to handle completion of item

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,19 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  name = 'Tay'
-  todoList = ['Add more todos']
-  newItem = ''
+  name: String
+  todos: any
+  doneTodos: any
+  newItem: String
 
   constructor() {
-
+    this.name = 'Tay'
+    this.todos = []
+    this.doneTodos = []
+    this.newItem = ''
   }
 
   ngOnInit() {
   }
 
-  updateItem(e: any) {
-    console.log(e.target.value)
+  onItemAdded() {
+    const todoObj = {
+      title: this.newItem,
+      completed: false
+    }
+    this.todos.push(todoObj)
+  }
+
+  onTodoDone(index) {
+    const doneTodo = this.todos[index]
+
+    this.todos.splice(index, 1)
+    this.doneTodos.push(doneTodo)
   }
 
 }
